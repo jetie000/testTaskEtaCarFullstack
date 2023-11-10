79ccd0ffcfdb7835819ead2d1079390d5da29d82
jetie000 <makeenko.005@gmail.com>
2023-11-11 00:32:11 +0300

    feat: add e2e tests

--- packages/client/cypress/e2e/modalInteraction.cy.ts
+++ packages/client/cypress/e2e/modalInteraction.cy.ts
@@ -27,4 +27,20 @@ describe('modal interaction', () => {
       cy.get('[data-testid="add-input-modal"] ~ button').should('exist').click();
       cy.get('[data-testid="add-response"]').should("contain", "Error")
     })
+
+    it('fav coin from table', () => {
+      cy.get('[data-testid="coin-table-body"]').get('tr:nth-child(2) th:first-child img').click()
+      cy.get('[data-testid="header-mycoins"] button').should('exist').click();
+      cy.get('[data-testid="purchase-mycoins"] > :nth-child(2) > :first-child').should('exist').click();
+      cy.get('[data-testid="purchase-mycoins"] > :nth-child(2) > :first-child').should('not.exist')
+    })
+
+    
+    it('fav coin from coin page', () => {
+      cy.get('[data-testid="coin-table-body"]').should("exist").children().get('tr:nth-child(2)').click()
+      cy.get('[data-testid="coin-page"] > :first-child > :first-child > :last-child').should('exist').click();
+      cy.get('[data-testid="header-mycoins"] button').should('exist').click();
+      cy.get('[data-testid="purchase-mycoins"] > :nth-child(2) > :first-child').should('exist').click();
+      cy.get('[data-testid="purchase-mycoins"] > :nth-child(2) > :first-child').should('not.exist')
+    })
   })
