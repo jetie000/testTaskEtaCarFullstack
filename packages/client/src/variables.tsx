80fdbcd21ddc349b0caad17c38267aa2f6347b07
jetie000 <makeenko.005@gmail.com>
2023-11-14 17:04:31 +0300

    fix: fix trpc url

--- packages/client/src/variables.tsx
+++ packages/client/src/variables.tsx
@@ -1,6 +1,7 @@
 export const variables = {
     API_URL : 'https://api.coincap.io/v2/',
-    MY_API_URL: 'http://localhost:8080/trpc',
+    // MY_API_URL: 'http://localhost:8080/trpc',
+    MY_API_URL: 'https://test-task-fullstack-server.vercel.app/trpc',
     COIN_ICONS_API_URL: 'https://coinicons-api.vercel.app/api/icon/',
     COINS_PER_PAGE: 50,
     COINS_PER_SEARCH: 6,
